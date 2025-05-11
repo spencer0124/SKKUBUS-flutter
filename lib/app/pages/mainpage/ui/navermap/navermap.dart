ab26cee9a1774e9eda36528b4d362e7907970c9a
spencer0124 <spencer0124@naver.com>
2025-05-11 10:27:05 +0900

    change: add Nmap controller, dynamic coords and cposition update

--- lib/app/pages/mainpage/ui/navermap/navermap.dart
+++ lib/app/pages/mainpage/ui/navermap/navermap.dart
@@ -4,6 +4,8 @@ import 'package:skkumap/app/pages/mainpage/ui/navermap/marker_bus.dart';
 import 'package:skkumap/app/pages/mainpage/ui/navermap/marker_campus.dart';
 import 'package:skkumap/app/pages/mainpage/ui/navermap/route_jongrobus.dart';
 import 'package:skkumap/app/types/campus_type.dart';
+import 'package:get/get.dart';
+import 'package:skkumap/app/pages/mainpage/ui/navermap/navermap_controller.dart';
 
 const initCameraPosition = NCameraPosition(
   target: NLatLng(37.587241, 126.992858),
@@ -12,7 +14,8 @@ const initCameraPosition = NCameraPosition(
   tilt: 50,
 );
 
-NaverMap buildMap() {
+Widget buildMap() {
+  final ultimateNampController = Get.put(UltimateNMapController());
   return NaverMap(
     options: const NaverMapViewOptions(
       zoomGesturesEnable: true,
@@ -26,31 +29,54 @@ NaverMap buildMap() {
     ),
     onMapReady: (mapcontroller) {
       mapcontroller.addOverlayAll({
-        ...buildCampusMarkers(CampusType.hssc),
-
-        // 종로 07 버스 마커는 일단 후순위로 미뤄두고, 주석처리한 상태!
-        // ...buildJongroBusMarkers(jongroBusPositions),
-        NMultipartPathOverlay(
-          id: "jongro07Route",
-          paths: [
-            const NMultipartPath(
-              color: Colors.green,
-              outlineColor: Colors.white,
-              coords: jongro07Route,
-            ),
-          ],
-        ),
-        NMultipartPathOverlay(
-          id: "jongro02Route",
-          paths: [
-            const NMultipartPath(
-              color: Colors.green,
-              outlineColor: Colors.white,
-              coords: jongro02Route,
-            ),
-          ],
-        ),
+        // 초기 마커 세팅
+        ...ultimateNampController.markers,
       });
+
+      // 마커 갱신
+      ever(ultimateNampController.markers, (_) {
+        mapcontroller.clearOverlays(); // 필요시 clear
+        mapcontroller.addOverlayAll({...ultimateNampController.markers});
+      });
+
+      // 카메라 갱신
+      ever<NCameraPosition>(ultimateNampController.cameraPosition, (pos) {
+        mapcontroller.updateCamera(
+          NCameraUpdate.withParams(
+            target: pos.target,
+            zoom: pos.zoom,
+            tilt: pos.tilt,
+            bearing: pos.bearing,
+          ),
+        );
+      });
+
+      // // 인사캠 건물 번호마커
+      // ...buildCampusMarkers(CampusType.hssc),
+
+      // // 종로07버스 노선 오버레이
+      // NMultipartPathOverlay(
+      //   id: "jongro07Route",
+      //   paths: [
+      //     const NMultipartPath(
+      //       color: Colors.green,
+      //       outlineColor: Colors.white,
+      //       coords: jongro07Route,
+      //     ),
+      //   ],
+      // ),
+
+      // // 종로02버스 노선 오버레이
+      // NMultipartPathOverlay(
+      //   id: "jongro02Route",
+      //   paths: [
+      //     const NMultipartPath(
+      //       color: Colors.green,
+      //       outlineColor: Colors.white,
+      //       coords: jongro02Route,
+      //     ),
+      //   ],
+      // ),
     },
   );
 }
